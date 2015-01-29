9d9d43a91f3c99abfac5b90f53e46e8dc947ea2d
Nathan Hall <nathan.mh@gmail.com>
2015-01-29 16:17:37 -0600

    iter_fib vs fibonacci_fib for tomorrows less

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,8 +1,22 @@
 require 'pry'
+
 def fibonacci(num)
   return 0 if num == 0
   return num if num <= 1
   fibonacci(num-1) + fibonacci(num-2)
+end
+
+def iter_fib(num)
+  return 0 if num == 0
+  return num if num <= 1
+  iter = 2
+  p = 1
+  q = 1
+  while iter <= num
+    p,q = q, p+q
+    iter +=1
+  end
+  return p
 end
 
 def factorial(num)
